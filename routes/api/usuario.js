e6ed98976fa57c08145d6199718f93e82ab0bb01
Camilost1404 <camitorres1404@gmail.com>
2020-12-19 22:31:24 -0500

    Configuracion final

--- routes/api/usuario.js
+++ routes/api/usuario.js
@@ -3,5 +3,10 @@ const usuarioController = require('../../controllers/UsuarioController');
 const auth = require('../../middlewares/auth');
 
 router.post('/login', usuarioController.login);
+router.get('/list', auth.verifyAdmin, usuarioController.list);
+router.post('/add', auth.verifyAdmin, usuarioController.add);
+router.put('/update', auth.verifyAdmin, usuarioController.update);
+router.put('/activate', auth.verifyAdmin, usuarioController.activate);
+router.put('/deactivate', auth.verifyAdmin, usuarioController.deactivate);
 
 module.exports = router;
